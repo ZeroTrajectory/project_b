dfe329c796cdaf66aee7e877e230c24dddcdec6f
guozicheng@bilibili.com <guozicheng@bilibili.com>
2021-05-25 23:59:49 +0800

    资源加载重构提交1

---
+++ Assets/Core/AssetManager/ABObject.cs
@@ -0,0 +1,28 @@
+﻿/*ABObject相当于一个AB加载器，一个object对应一个AB，具有以下职能
+1.记录AB的引用计数
+2.记录AB的加载实例
+3.记录AB加载的回调
+4.记录AB的依赖AB
+*/
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Load 
+{
+    enum LoadStatus
+    {
+        Wait,
+        Loading,
+        Loaded,
+        WaitDelete,
+        Deleted,
+    }
+    public class ABObject
+    {
+        private LoadStatus _status;
+        private int _refCount;
+        private AssetBundle _ab;
+        private List<ABObject> _depends = new List<ABObject>();
+    }
+}
+
--- Assets/Script/TestLoader.cs
+++ Assets/Script/TestLoader.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using Core.Load;
 using UnityEngine;
 using UnityEngine.UI;
 using Sirenix.OdinInspector;
