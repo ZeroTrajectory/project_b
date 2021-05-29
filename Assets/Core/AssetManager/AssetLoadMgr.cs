6e74f5606d57727bcf90feace71d1f6961725029
guozicheng <guozicheng@bilibili.com>
2021-05-29 16:23:10 +0800

    资源加载重构提交4

---
+++ Assets/Core/AssetManager/AssetLoadMgr.cs
@@ -0,0 +1,11 @@
+﻿/*
+AssetLoadMgr是资源加载卸载的唯一对外接口，传入加载的相对路径地址，加载的资源类型，回调加载完成
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetLoadMgr : MonoBehaviour
+{
+
+}
